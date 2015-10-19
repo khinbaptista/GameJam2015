372a363fe8dc6d35f348dd49af28a1ab3977833f
Marcelo Vasques <msouvas@gmail.com>
2015-10-19 20:27:01 -0200

    Script for AI movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour, IHitable {
+public abstract class Enemy : MonoBehaviour, IHitable {
 
 	[SerializeField]
 	private float _HP;
+	public float moveRate;
+	public float damageAmount;
 
 	public float HP {
 		get { return _HP; }
---
+++ Assets/Scripts/MovementAI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class MovementAI : MonoBehaviour {
+	// 1 - Designer variables
+
+	public float moveSpeed;
+	public float maxDistance;
+	public Transform target;
+
+	void Start () {
+		target = GameObject.Find("hero").transform;
+	}
+	
+	void Update() {
+		if (Vector3.Distance(target.position, transform.position) > maxDistance)
+		{
+			// Get a direction vector from us to the target
+			Vector3 dir = target.position - transform.position;
+			
+			// Normalize it so that it's a unit direction vector
+			dir.Normalize();
+			
+			// Move ourselves in that direction
+			transform.position += dir * moveSpeed * Time.deltaTime;
+		}
+	}
+}
