57a3ef3f71e1e83a7c0605378e64539a0bbf073d
Khin Baptista <kerbaptista@inf.ufrgs.br>
2015-10-20 10:55:53 -0200

    Implemented poison and added scenes to gitignore

---
+++ Assets/Scripts/Poison.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Poison : MonoBehaviour {
+
+	[SerializeField]
+	private float max = 100;
+
+	[SerializeField] [Tooltip("this * poison% / s")]
+	private float damageOverTime;
+
+	[SerializeField] [Tooltip("this * poison% / attack")]
+	private float damageBonus;
+
+	[SerializeField] [Tooltip("this * poison%")] [Range(1f, 5f)]
+	private float speedBonus;
+
+	private Animator anim;
+	
+	private float poisonLevel;
+	public float PoisonLevel {
+		set {
+			poisonLevel = Mathf.Clamp(value, 0f, max);
+			UpdateAnimationSpeed();
+		}
+
+		get { return poisonLevel; }
+	}
+
+	public float PoisonLevelScaled {
+		get { return PoisonLevel / max; }
+	}
+
+	public float DamageBonus {
+		get {	return damageBonus * PoisonLevelScaled;	}
+	}
+
+	public float SpeedBonus {
+		get { return speedBonus * PoisonLevelScaled; }
+	}
+
+	public void AddPoison(float deltaPoison) {
+		PoisonLevel += deltaPoison;
+	}
+
+	private void UpdateAnimationSpeed() {
+		anim.speed = 1 + SpeedBonus;
+	}
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
