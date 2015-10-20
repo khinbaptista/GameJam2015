7b434bd659b1d2cce6371c74468c9f61396c2794
João <jgaribaldijunior@gmail.com>
2015-10-20 21:03:38 -0200

    Adionado contador de poção, Current HP em player e referenciado player em HP bar

--- Assets/Scripts/HPBarScript.cs
+++ Assets/Scripts/HPBarScript.cs
@@ -3,20 +3,19 @@ using System.Collections;
 using UnityEngine.UI;   
 
 public class HPBarScript : MonoBehaviour {
-    public float HPMax;
-    public float CurrentHP;
+    public Player player;
     public Image HPBar;
-    public Image DamageBar;
     private float _imageMaxSize;
     // Use this for initialization
-    void Start () {
+    void Awake () {
         this._imageMaxSize = HPBar.rectTransform.sizeDelta.x;
+        this.player = GameObject.FindObjectOfType<Player>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        float imageCurrentSize = this._imageMaxSize * this.CurrentHP / HPMax;
+        float imageCurrentSize = this._imageMaxSize * this.player.CurrentHP / this.player.HP;
 
         HPBar.rectTransform.sizeDelta =
             new Vector2(
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,28 @@ using System.Collections;
 public class Player : MonoBehaviour, IHitable {
 	
 	[SerializeField]
-	private float _HP;
-	private Animator animator;
-	public float moveRate;
+	private float _HP = 4000;
+    private float _currentHP;
+    private Animator animator;
+    private int _potionAmount;
+    public float moveRate;
 	public float damageAmount;
 	public bool isAttacking = false;
 	
 	public float HP {
 		get { return _HP; }
+        set { _HP = value; }
 	}
-	
-	// Use this for initialization
-	void Start () {
-		animator = GetComponent<Animator> ();
+
+    public float CurrentHP {
+        get { return _currentHP; }
+        set { _currentHP = value; }
+    }
+
+    // Use this for initialization
+    void Start () {
+        this._currentHP = this._HP;
+        animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -24,9 +33,11 @@ public class Player : MonoBehaviour, IHitable {
 				
 	}
 	
+
 	public void OnHit(float attackPower) {
 		_HP -= attackPower;
 		if (_HP < 0) {
+            _HP = 0;
 			animator.SetTrigger("Death");
 		}
 	}
