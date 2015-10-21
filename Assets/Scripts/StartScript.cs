f5815836a5a892d202ce831f4fd5ff3c241b5edd
João <jgaribaldijunior@gmail.com>
2015-10-21 10:32:15 -0200

    adicionado opcao de começar o jogo e script na cena de mainmenu, adicionado prefab de potion UI usado no canvas principal

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                Application.LoadLevel("forestScene");
+            }
+        }
+
+    }
+
+}
