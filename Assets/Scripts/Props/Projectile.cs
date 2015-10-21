6258af6cd554a6ce5a006aa8eb7439416ffe5237
Khin Baptista <kerbaptista@inf.ufrgs.br>
2015-10-21 16:45:38 -0200

    Created projectile

---
+++ Assets/Scripts/Props/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	[SerializeField]
+	private float speed;
+
+	[SerializeField]
+	private float damage;
+
+	[Tooltip("Leave Z component alone!")]
+	public Vector3 direction;
+
+	void Start () {
+	}
+	
+	void Update () {
+		transform.position += direction * speed * Time.deltaTime;
+	}
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		IHitable victim = other.GetComponent<IHitable>();
+
+		if (victim != null) {
+			victim.OnHit(damage);
+		}
+
+		Destroy(gameObject);
+	}
+}
