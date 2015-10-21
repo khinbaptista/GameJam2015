ab49b0571d07a73f169d1890d7139bbac69ad732
João <jgaribaldijunior@gmail.com>
2015-10-21 20:47:40 -0200

    Adicionada cena de introdução

---
+++ Assets/Scripts/MovementIntroAI.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class MovementIntroAI : MonoBehaviour
+{
+    // 1 - Designer variables
+
+    public float moveSpeed;
+    public float maxDistance;
+    public float minDistance;
+    public Transform target;
+    public Animator animator;
+    public EnemyAttack attack;
+    public bool facingRight = false;
+    private bool _isGrounded = false;
+    
+
+    public bool isGrounded
+    {
+        get { return _isGrounded; }
+    }
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        
+        
+        Flip();
+
+        MovementIntroAI mv = GetComponentInChildren<MovementIntroAI>();
+        mv.enabled = false ;
+    }
+
+    void Update()
+    {
+
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (transform.position != target.position)
+        {
+            Vector3 dir = target.position - transform.position;
+            dir.Normalize();
+            transform.position += dir * moveSpeed * Time.deltaTime;
+            animator.SetFloat("Speed", 0.4f);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0f);
+
+            if (distance < maxDistance)
+                attack.AttackStart();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/TextTyperScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextTyperScript : MonoBehaviour
+{
+
+    private bool _endText = false;
+    public GameObject speech;
+    public float letterPause = 0.2f;
+    public AudioClip typeSound1;
+    public AudioClip typeSound2;
+
+    string message;
+    Text textComp;
+
+    // Use this for initialization
+    void Start()
+    {
+        textComp = GetComponent<Text>();
+        message = textComp.text;
+        textComp.text = "";
+            StartCoroutine(TypeText());
+        
+    }
+
+    void Update()
+    {
+        if (_endText)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                
+                speech.SetActive(false);
+
+                GameObject character = GameObject.Find("Character");
+                MovementIntroAI mv = character.GetComponentInChildren<MovementIntroAI>();
+                mv.enabled = true;
+                mv.SendMessage("Flip");
+                GameObject.Find("PlayerCamera").GetComponentInChildren<SmoothFollow>().enabled = false;
+            }
+        }
+    }
+    IEnumerator TypeText()
+    {
+        foreach (char letter in message.ToCharArray())
+        {
+            textComp.text += letter;
+            yield return new WaitForSeconds(letterPause);
+        }
+        _endText = true;
+    }
+}
