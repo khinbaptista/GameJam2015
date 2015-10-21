408acb9589f168e1a12aed771c3bb82a09324bf6
Khin Baptista <kerbaptista@inf.ufrgs.br>
2015-10-21 14:49:34 -0200

    Created instant kill for pits + fixed special attack end

---
+++ Assets/Scripts/InstaKill.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstaKill : MonoBehaviour {
+
+	private Collider2D area;
+
+	void Start () {
+		area = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		IHitable victim = other.gameObject.GetComponent<IHitable>();
+
+		if (victim == null)
+			return;
+
+		victim.OnHit(victim.CurrentHp);
+	}
+}
