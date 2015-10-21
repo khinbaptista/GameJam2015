efcec1e485396a405976f23ea65479807c2c521b
Khin Baptista <kerbaptista@inf.ufrgs.br>
2015-10-21 15:31:34 -0200

    Poison now drains life and increases attack speed (on ground)

--- Assets/Scripts/Poison.cs
+++ Assets/Scripts/Poison.cs
@@ -7,16 +7,23 @@ public class Poison : MonoBehaviour {
 	private float max = 100;
 
 	[SerializeField] [Tooltip("this * poison% / s")]
-	private float damageOverTime;
+	private float damageOverTime = 5;
 
 	[SerializeField] [Tooltip("this * poison% / attack")]
-	private float damageBonus;
+	private float damageBonus = 5;
 
 	[SerializeField] [Tooltip("this * poison%")] [Range(1f, 5f)]
 	private float speedBonus = 2f;
 
 	private Animator anim;
-	
+	private Player player;
+
+	[SerializeField][Tooltip("Amount of poison increased at once")]
+	private float deltaPoison = 5;
+
+	[Tooltip("Amount of poison decreased per second")]
+	public float poisonDecreaseRate = 5;
+
 	[SerializeField] [Range(0, 100)]
 	private float poisonLevel;
 	public float PoisonLevel {
@@ -40,8 +47,8 @@ public class Poison : MonoBehaviour {
 		get { return speedBonus * PoisonLevelScaled; }
 	}
 
-	public void AddPoison(float deltaPoison) {
-		PoisonLevel += deltaPoison;
+	public void AddPoison(float _deltaPoison) {
+		PoisonLevel += _deltaPoison;
 	}
 
 	private void UpdateAnimationSpeed() {
@@ -51,10 +58,21 @@ public class Poison : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
+		player = GetComponent<Player>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown("Poison")) {
+			AddPoison(deltaPoison);
+		}
+
+		if (poisonLevel > 0.0f) {
+			player.OnHit(damageOverTime * Time.deltaTime);
+			PoisonLevel -= poisonDecreaseRate * Time.deltaTime;
+
+			if (poisonLevel < 0)
+				poisonLevel = 0.0f;
+		}
 	}
 }
