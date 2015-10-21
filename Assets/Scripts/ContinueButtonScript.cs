7ffc64ac595bb0658a5fd54466173db17e08e047
João <jgaribaldijunior@gmail.com>
2015-10-21 11:29:37 -0200

    alterado canvas. Adicionados botoes, script de game over, script de voltar para o menu

---
+++ Assets/Scripts/ContinueButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ContinueButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponentInChildren<Text>().text = "Continue";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScript : MonoBehaviour {
+    private Player _player;
+	// Use this for initialization
+	void Start () {
+        this._player = (Player)FindObjectOfType(typeof(Player));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_player.isDead)
+        {
+            foreach(GameObject go in transform)
+            {
+                go.gameObject.SetActive(true);
+            }
+        }
+	}
+}
