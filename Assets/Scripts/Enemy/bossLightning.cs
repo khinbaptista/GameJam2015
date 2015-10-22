06a7db20963f8f3f2630d3439f29792c01a3aa8a
Marcelo Vasques <msouvas@gmail.com>
2015-10-22 10:29:28 -0200

    Fixing boss special damage

--- Assets/Scripts/Enemy/bossLightning.cs
+++ Assets/Scripts/Enemy/bossLightning.cs
@@ -8,31 +8,34 @@ public class bossLightning : MonoBehaviour {
 	private Player player;
 	private Animator animator;
 	private Enemy enemy;
+	private GameObject character;
+	private Transform myTransform;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find("Character").GetComponent<Player>();
 		animator = GetComponent<Animator> ();
 		enemy = GetComponentInParent<Enemy> ();
+		character = GameObject.Find ("Character");
+		myTransform = this.gameObject.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if ((Random.value > 0.993) && !isActive && !enemy.IsDead) {
-			this.gameObject.transform.position = gameObject.transform.parent.TransformPoint (Random.Range(-5, 5), this.gameObject.transform.parent.position.y + 10, 0);
+			myTransform.position = gameObject.transform.parent.TransformPoint (Random.Range(-5, 5), myTransform.parent.position.y + 10, 0);
 			isActive = true;
 			animator.SetBool("Active", true);
-
-			if(this.gameObject.transform.position.x > (GameObject.Find("Character").transform.position.x - 1)) {
-				if(this.gameObject.transform.position.x < (GameObject.Find("Character").transform.position.x + 1)) {
-					player.OnHit(lightDamage);
-				}
-			}
 		}
 	}
 
 	public void LightningEnd() {
 		isActive = false;
+		if(myTransform.position.x > (character.transform.position.x - 1)) {
+			if(myTransform.position.x < (character.transform.position.x + 1)) {
+				player.OnHit(lightDamage);
+			}
+		}
 		animator.SetBool("Active", false);
 	}
 }
