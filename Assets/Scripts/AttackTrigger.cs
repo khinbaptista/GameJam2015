78d409558449d16221987d5b58eff65336890159
Khin Baptista <kerbaptista@inf.ufrgs.br>
2015-10-21 22:01:12 -0200

    Poison now influences attack power + introducing poison VFX (not implemented)

--- Assets/Scripts/AttackTrigger.cs
+++ Assets/Scripts/AttackTrigger.cs
@@ -5,9 +5,11 @@ public class AttackTrigger : MonoBehaviour {
 
 	public float attackPower;
 
+	private Poison poison;
+
 	// Use this for initialization
 	void Start () {
-	
+		poison = GetComponent<Poison>();
 	}
 	
 	// Update is called once per frame
@@ -21,6 +23,6 @@ public class AttackTrigger : MonoBehaviour {
         if (gameObject.CompareTag(other.gameObject.tag)) return;
 
         if (!enemy.IsDead)
-			enemy.OnHit(attackPower);
+			enemy.OnHit(attackPower + poison.DamageBonus);
 	}
 }
