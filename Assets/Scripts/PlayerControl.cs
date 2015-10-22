7d6924fb4194911281ce634581546df2af033c1d
João <jgaribaldijunior@gmail.com>
2015-10-22 11:03:53 -0200

    Player modificado

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,7 +9,8 @@ public class PlayerControl : MonoBehaviour
 	public bool firstJump = false;				// Condition for whether the player should jump.
 	public bool secondJump = false;
 	public bool canSecondJump = true;
-	
+    private float h;
+    private float speed;
 	public float moveForce = 0.1f;			// Amount of force added to move the player left and right.
 	public float maxSpeed = 3f;				// The fastest the player can travel in the x axis.
 	public float jumpForce = 100f;			// Amount of force added when the player jumps.
@@ -25,7 +26,6 @@ public class PlayerControl : MonoBehaviour
 	void Awake()
 	{
 		// Setting up references.
-		groundCheck = transform.Find("groundCheck");
 		animator = GetComponent<Animator>();
 		rigidbody2d = GetComponent<Rigidbody2D>();
 		playerHealth = GetComponent<Player>();
@@ -34,12 +34,20 @@ public class PlayerControl : MonoBehaviour
 	
 	void Update()
 	{
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
-		animator.SetBool ("Grounded", grounded);
+        
+        // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+        // Cache the horizontal input.
+        h = Input.GetAxis("Horizontal");
+        // The Speed animator parameter is set to the absolute value of the horizontal input.
+        animator.SetFloat("Speed", speed);
 
-		// If the jump button is pressed and the player is grounded then the player should jump.
-		if(Input.GetButtonDown("Jump") && grounded)
+
+        
+
+        // If the player should jump...
+
+        // If the jump button is pressed and the player is grounded then the player should jump.
+        if (Input.GetButtonDown("Jump") && grounded)
 			firstJump = true;
 		if (Input.GetButtonDown ("Jump") && !grounded && canSecondJump) {
 			secondJump = true;
@@ -49,58 +57,55 @@ public class PlayerControl : MonoBehaviour
 		if (grounded && !canSecondJump) {
 			canSecondJump = true;
 		}
-	}
+
+        if (firstJump)
+        {
+            // Set the Jump animator trigger parameter.
+            makeJump();
+
+            // Make sure the player can't jump again until the jump conditions from Update are satisfied.
+            firstJump = false;
+        }
+        else if (secondJump)
+        {
+            makeJump();
+            secondJump = false;
+        }
+    }
 	
 	
 	void FixedUpdate ()
 	{
-		// Cache the horizontal input.
-		float h = Input.GetAxis("Horizontal");
-		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		animator.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
-
-        		
-		// If the input is moving the player right and the player is facing left...
-		if(h > 0 && !facingRight)
-			// ... flip the player.
-			Flip();
-		// Otherwise if the input is moving the player left and the player is facing right...
-		else if(h < 0 && facingRight)
-			// ... flip the player.
-			Flip();
+        speed = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x);
+        
+        // If the input is moving the player right and the player is facing left...
+        if (h > 0 && !facingRight)
+            // ... flip the player.
+            Flip();
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (h < 0 && facingRight)
+            // ... flip the player.
+            Flip();
 
         // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
         //rigidbody2d.velocity = Vector2.right * h * moveForce;
 
-	    if (h > 0 || 0 > h)
-	    {
-	        Vector2 currentY = Vector2.zero;
-	        currentY.y = rigidbody2d.velocity.y;
-	        rigidbody2d.velocity = (Vector2.right*h*moveForce) + currentY;
-	    }
-	    else
-	    {
+        if (h > 0 || 0 > h)
+        {
+            Vector2 currentY = Vector2.zero;
+            currentY.y = rigidbody2d.velocity.y;
+            rigidbody2d.velocity = (Vector2.right * h * moveForce) + currentY;
+        }
+        else
+        {
             rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
         }
 
-	    // If the player should jump...
-        if (firstJump)
-		{
-			// Set the Jump animator trigger parameter.
-			makeJump ();
-				
-			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
-			firstJump = false;
-		} 
-		else if(secondJump)
-		{
-			makeJump ();
-			secondJump = false;
-		}
-	}
 
-	void LateUpdate() {
-		if (playerHealth.IsDead)
+    }
+
+    void LateUpdate() {
+        if (playerHealth.IsDead)
 			enabled = false;
 	}
 	
@@ -121,6 +126,24 @@ public class PlayerControl : MonoBehaviour
 		animator.SetTrigger ("Jump");
         rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpForce);
     }	
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            grounded = true;
+            animator.SetBool("Grounded", true);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            grounded = false;
+            animator.SetBool("Grounded", false);
+        }
+    }
 
 }
 		
