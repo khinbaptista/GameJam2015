24804757d201eed63a6032eb2ad7bf4623ae6b5f
João <jgaribaldijunior@gmail.com>
2015-10-23 00:19:12 -0200

    scripts relocados para a pasta certa

---
+++ Assets/Scripts/WhoSavesScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class WhoSavesScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject toSave = GameObject.Find("ToSave");
+        GameObject savior = GameObject.Find("Savior");
+        string pathMale = "Assets/Animations/Characters/Female/FemaleAC.controller";
+        string pathFemale = "Assets/Animations/Characters/Male/MaleAC.overrideController";
+        Animator animToSave = toSave.GetComponentInChildren<Animator>();
+        Animator animSavior = savior.GetComponentInChildren<Animator>();
+
+        if (PlayerPrefs.GetInt("Payer") == 0)
+        {
+            animToSave.runtimeAnimatorController =
+                (RuntimeAnimatorController) AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>(pathFemale);
+            animSavior.runtimeAnimatorController =
+                (RuntimeAnimatorController)AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>(pathMale);
+        }
+        else
+        {
+            animToSave.runtimeAnimatorController =
+                (RuntimeAnimatorController)AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>(pathMale);
+            animSavior.runtimeAnimatorController =
+                (RuntimeAnimatorController)AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>(pathFemale);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
