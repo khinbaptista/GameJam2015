6f4e84c8ad6bf429b632c8042fea5f1e03905a36
João <jgaribaldijunior@gmail.com>
2015-10-24 21:43:17 -0200

    UPDATE

---
+++ Assets/Scripts/IdentifierScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IdentifierScript : MonoBehaviour {
+    public bool savior = false;
+    public Image saviorImage;
+    public Image toSaveImage;
+
+    // Use this for initialization
+    void Start() {
+        
+        if (PlayerPrefs.GetInt("Player") == 0)
+        {
+            toSaveImage = saviorImage;
+            saviorImage = GameObject.Find("HerHead").GetComponentInChildren<Image>();
+
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        if (savior)
+        {
+            saviorImage.gameObject.SetActive(true);
+            toSaveImage.gameObject.SetActive(false);
+        }
+        else
+        {
+            saviorImage.gameObject.SetActive(false);
+            toSaveImage.gameObject.SetActive(true);
+        }
+	}
+	
+}
---
+++ Assets/Scripts/WhoSpeakScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhoSpeakScript : MonoBehaviour {
+    public bool savior;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        IdentifierScript identifier = GameObject.Find("Speech2").GetComponentInChildren<IdentifierScript>();
+        if (this.savior)
+            identifier.savior = true;
+        else
+            identifier.savior = false;
+    }
+}
